13a08c61812ced6f901f3c9bc88983b08740a569
kuangfg <1125303286@qq.com>
2025-08-29 17:34:55 +0800

    ✨ feat: 头部布局

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,5 +1,17 @@
+import { ShoppingCart, UserIcon } from 'lucide-react';
+import Link from 'next/link';
+
 const Header = () => {
-  return <>Header</>;
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link href="/" className="flex-start">
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
